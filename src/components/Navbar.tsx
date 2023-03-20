168b7c18cf587edd1d6e0a52ba1345d5cdb4f105
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-20 19:53:46 +0530

    feat :add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { HStack, Image, Text } from "@chakra-ui/react"
+
+const Navbar = () => {
+    return (
+        <HStack>
+            {/* <Image src={} /> */}
+            <Text>Navbar</Text>
+        </HStack>
+    )
+}
+export default Navbar
