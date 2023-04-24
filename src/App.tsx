765d5fbdac97127ab961fff6cd01923e91b4d92b
lokesh <lokeshkhati005@gmail.com>
2023-04-24 22:13:19 +0530

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Navbar } from "./components";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav" "nav" "aside" "main"`
+      }}>
+      <GridItem pl="2" bg="orange.300" area="nav">
+        <Navbar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
+};
+export default App;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,12 @@
+import { useEffect, useState } from "react"
+
 const useGames = () => {
     const [games, setGames] = useState([])
     const [error, setError] = useState('')
 
-    useEffect(() => {
-        apiClient.get('/games').then((res.data.results) => setGames(res)).catch ((error) => SpeechSynthesisErrorEvent(error.message))
-    }, [])
+    // useEffect(() => {
+    //     apiClient.get('/games').then((res.data.results) => setGames(res)).catch ((error) => console.log(error.message))
+    // }, [])
 return { games, error }
 }
 export default useGames
