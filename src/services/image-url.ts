889a25522bb2979431273b825221e86a1a3c72f1
lokesh <lokeshkhati005@gmail.com>
2023-05-05 10:21:49 +0530

    refactor code

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,10 +1,3 @@
-// const grtCroppedImageUrl = (url: string) => {
-//     const target = 'media/';
-//     const index = url.indexOf(target) + target.length
-//     return url.slice(0, index) + 'crop/600/400' + url.slice(index)
-// }
-// export { grtCroppedImageUrl }
-
 
 const getCroppedImageUrl = (url: string) => {
     // if (!url) return null;
