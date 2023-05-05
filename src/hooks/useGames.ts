ae842a28f937802f9a8703872b4f393956eeef90
lokesh <lokeshkhati005@gmail.com>
2023-05-05 22:25:28 +0530

    customize theme

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,7 +11,8 @@ export interface Game {
     name: string,
     background_image: string;
     parent_platforms: { platform: Platform }[];
-    metacritic: number
+    metacritic: number;
+    rating_top: number;
 }
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
     params: {
