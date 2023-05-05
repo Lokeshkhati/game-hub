9936eb11f38c0e343c66a4267ba676012b440547
lokesh <lokeshkhati005@gmail.com>
2023-05-05 10:23:05 +0530

    refactor

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,7 +5,8 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params: {
-        key: 'b1fb28f3ce904e90b1ffe096974225bc'
+        // key: 'b1fb28f3ce904e90b1ffe096974225bc'
+        key: 'c542e67aec3a4340908f9de9e86038af'
     }
 })
 
