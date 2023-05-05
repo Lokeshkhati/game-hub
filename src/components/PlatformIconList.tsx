de5fa5582b7380ce98df964eac3a5f7a4912cd32
lokesh <lokeshkhati005@gmail.com>
2023-05-05 10:24:17 +0530

    refactor

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { Icon, HStack } from "@chakra-ui/react"
-import { Platform } from "../hooks/useGames"
+import { Platform } from '../hooks/useGames';
 import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa'
 import { MdPhoneIphone } from 'react-icons/md'
 import { SiNintendo } from 'react-icons/si'
@@ -24,7 +24,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     }
     return (
         <HStack marginY={1}> {
-            platforms.map((platform) => <Icon as={iconMap[platform.slug]} color='gray.500' />)
+            platforms.map((platform) => <Icon key={platform?.id} as={iconMap[platform.slug]} color='gray.500' />)
         }
         </HStack>
     )
