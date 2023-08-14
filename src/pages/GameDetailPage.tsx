b06f6a9f89cd84851ef70c8501b059b8dc790c32
lokesh <lokeshkhati005@gmail.com>
2023-08-14 06:09:25 +0530

    add game screen shots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -6,6 +6,7 @@ import DefinitionItem from "../components/DefinitionItem"
 import CriticScore from "../components/CriticScore"
 import GameAttributes from "../components/GameAttributes"
 import GameTrailer from "../components/GameTrailer"
+import GameScreenshots from '../components/GameScreenshots';
 
 const GameDetailPage = () => {
     const { slug } = useParams()
@@ -19,6 +20,7 @@ const GameDetailPage = () => {
             <ExpandableText >{game.description_raw}</ExpandableText >
             <GameAttributes game={game} />
             <GameTrailer gameId={game.id} />
+            <GameScreenshots gameId={game.id} />
         </>
     )
 }
